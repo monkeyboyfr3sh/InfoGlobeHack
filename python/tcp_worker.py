680def592691a976ba347b8e23471c234d85eab4
David Cain <d.cain2740@gmail.com>
2023-08-27 17:37:03 -0500

    Update tcp_worker.py

--- python/tcp_worker.py
+++ python/tcp_worker.py
@@ -8,28 +8,6 @@ from PyQt5.QtCore import Qt, QObject, pyqtSignal, pyqtSlot, QThread
 from file_helper import get_file_total_bytes
 from tcp_client import InfoGlobeController
 
-def globe_action(globe: InfoGlobeController):
-    # blink_idx = 0xc
-    blink_idx = 0x0
-    shift_idx = 13
-
-    tx_data = bytes([0x05,0x00])
-    tx_data += "< Booting >".encode()
-    # tx_data += "New ".encode()
-    # tx_data += hex(blink_idx).encode()
-    # tx_data += " New".encode()
-    tx_data += bytes([0x0])
-    tx_data += bytes([blink_idx,shift_idx])
-    globe.send_bytes(tx_data)
-    # time.sleep(2)
-
-    # globe("hell11o")
-    # globe.set_text_single_scroll_left("Connecting...")
-    # globe.set_text_single_scroll_left("\x81")
-    # globe.set_text_stationary_front("my_string")
-    # globe.set_text_stationary_right("my_string")
-    # globe.instant_blank()
-
 class TcpConnectWorker(QObject):
     finished = pyqtSignal()  # Signal to indicate that the worker has finished
     connect_status = pyqtSignal(int)  # Signal to indicate that the worker has finished
